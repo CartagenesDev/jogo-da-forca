18a124faa633cc3bf6e8d2b8bf37f41560050ab6
cartagenes <junior_pjl@hotmail.com>
2024-04-14 16:27:46 -0300

    versao_01

---
+++ forca.py
@@ -0,0 +1,60 @@
+from boneco_forca import desenhar_boneco
+
+
+palavra = "python"
+letra_usuario = []
+chances = 5
+erros = 0
+ganhou = False
+
+while chances >= 0:  
+    ganhou = all(letra.lower() in letra_usuario for letra in palavra)
+    """ganhou = True
+    for letra in palavra:
+        if letra.lower() not in letra_usuario:
+            ganhou = False"""
+
+    if ganhou: #or chances == 0:
+        break  
+    #logica do jogo    
+    for letra in palavra:        
+        if letra.lower() in letra_usuario:
+            print(letra, end= " ")
+
+        else:
+            print("_", end= " ")
+    print("\n") 
+
+    tentativa = input("Digite uma letra: ").lower().strip() # Converte para minúsculas e remove espaços extras   
+
+    if len(tentativa) != 1 or not tentativa.isalpha():  # Verifica se a entrada é válida
+        print("Por favor, digite apenas uma letra.")
+        continue
+ 
+
+    if tentativa in letra_usuario:
+        print(f"Você ja digitou a letra: {tentativa}")
+
+    else:
+
+        letra_usuario.append(tentativa)
+
+        if tentativa not in palavra:         
+            erros += 1
+            print(f"\nVocê errou, você tem {chances} chances")
+            desenhar_boneco(erros)
+            chances -= 1
+        else:
+            
+            print(f"Você acertou uma letra! ") 
+            
+    
+             
+ 
+if ganhou:
+    print(f"Parabens vocẽ ganhou o jogo. A palavra era: {palavra}")
+
+else:
+    print(f"Você perdeu! A palavra era {palavra}")    
+
+  
